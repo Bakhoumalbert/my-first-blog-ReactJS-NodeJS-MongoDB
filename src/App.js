a857edd68aaedf5444514840ccbea31edb624954
Bakhoumalbert <bakhoum.albert@uam.edu.sn>
2023-03-04 10:58:04 +0000

    Ajout de moi premier blog

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Register from "./pages/register/Register.jsx";
 import Settings from "./pages/settings/Settings.jsx";
 import Single from "./pages/single/Single.jsx";
 import Write from "./pages/write/Write.jsx";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div >
+    <Router >
       <TopBar />
-      <Login />
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
 
   );
 }
